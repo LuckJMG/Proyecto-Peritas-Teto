beb6b5d7ae17f0c4186e011ce9a3c7bc67eff5e3
JorgeJaceval <aceval481@gmail.com>
2025-11-11 17:55:00 -0300

    Creación de ListaUsuarios (SuperAdmin)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import ListaCondominios from "@/pages/super-admin/ListaCondominios"
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import ListaCondominios from "@/pages/super-admin/ListaCondominios";
+import ListaUsuarios from "@/pages/admin/ListaUsuarios";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/condominios" element={<ListaCondominios />} />
+        <Route path="/usuarios" element={<ListaUsuarios />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
